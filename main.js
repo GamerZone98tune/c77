ec538d470b857d1c43ad830a686082403828838d
GamerZone98tune <85111280+GamerZone98tune@users.noreply.github.com>
2021-08-20 16:56:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var fruits=["APPLE","ORANGE","MANGO","BLUEBERRY","GRAPES","RASBERRY","WATERMELON"];
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+console.log(fruits.length);
+fruits.sort();
+var i=0;
+for(i=0;i<fruits.length;i++){
+    console.log(fruits[i]);
+}
+fruits.push("AVOCADO");
+console.log(fruits);
